41096f9efce3cc297e3d4fa4dfd405e681d2c57b
Felipe Madalengoitia <118880777+FelipeRMP@users.noreply.github.com>
2025-11-24 14:13:46 -0500

    Modify admin constructor to include tipo_usuario

--- src/main/java/proyecto_poo/modelo/usuario/admin.java
+++ src/main/java/proyecto_poo/modelo/usuario/admin.java
@@ -3,8 +3,8 @@ package proyecto_poo.modelo.usuario;
 public class admin extends usuario {
 
     //constructor
-    public admin(String nombre_usuario, String clave) {
-        super(nombre_usuario, clave);
+    public admin(String nombre_usuario, String clave, tipo_usuario tipo) {
+        super(nombre_usuario, clave, tipo);
     }
 
     //getter
